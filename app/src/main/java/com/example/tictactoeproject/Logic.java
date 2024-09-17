5cbeba66880c1100bac7b64c8137a288b492a627
noa1107 <noadachbash@gmail.com>
2024-09-17 10:51:53 +0300

    second commit

--- app/src/main/java/com/example/tictactoeproject/Logic.java
+++ app/src/main/java/com/example/tictactoeproject/Logic.java
@@ -3,17 +3,73 @@ package com.example.tictactoeproject;
 public class Logic {
 
     private int player;
-    private  int[][] board;
+    private int counter;
+    private int[][] board;
 
     public Logic() {
+
+        //1 = x, -1 = 0
         player = 1;
+        counter = 0;
         board = new int[3][3];
     }
 
-    public void userInput(int x, int y){
-        if(board[x][y] == 0){
+    public void updateBoard(int x, int y){
+        if(isEmpty(x,y)){
+            counter++;
             board[x][y] = player;
-
+            player*=-1;
         }
+        int curr_state = checkWin(x,y);
+        if(curr_state != 0){
+           gameover(curr_state);
+        }
+        else if(counter == 9) {
+            gameover(0);
+        }
+
+    }
+
+
+
+    public boolean isEmpty(int x, int y){
+        return board[x][y] == 0;
+    }
+
+    public int checkWin(int x, int y){
+        int rowSum = 0;
+        for (int j = 0; j < 3; j++) {
+            rowSum += board[j][y];
+        }
+        int colSum = 0;
+        for(int j = 0; j < 3; j++){
+            colSum += board[x][j];
+        }
+        int alexson = 0;
+        int i = 0;
+        for(int j = 0; j<3; j++){
+            alexson+=board[i][j];
+            i++;
+        }
+        int alexson2 = 0;
+        i = 2;
+        for(int j = 2; j>=0; j--){
+            alexson2+=board[i][j];
+            i--;
+        }
+        if(rowSum == 3 || colSum == 3 || alexson == 3 || alexson2 == 3){
+            return 1;
+        }
+        else if(rowSum == -3 || colSum == -3 || alexson == -3 || alexson2 == -3){
+            return -1;
+        }
+        return 0;
+    }
+    public int gameover(int winner){
+        return winner;
+    }
+
+    public int getPlayer() {
+        return player;
     }
 }
--- app/src/main/java/com/example/tictactoeproject/MainActivity.java
+++ app/src/main/java/com/example/tictactoeproject/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.tictactoeproject;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Logic logic = new Logic();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,24 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void updateBoard(View view) {
+        String clicked_id = getResources().getResourceEntryName(view.getId());
+        int x = clicked_id.charAt(5) - '0';
+        int y = clicked_id.charAt(6) - '0';
+
+        logic.updateBoard(x, y);
+
+        TextView player_display = (TextView) findViewById(R.id.player_display);
+        player_display.setText(String.valueOf(logic.getPlayer()));
+
+        ImageView clicked_image= (ImageView) findViewById(view.getId());
+        int curr_player = logic.getPlayer();
+        if (curr_player == 1) {
+            clicked_image.setImageResource(R.drawable.blue_x);
+        } else {
+            clicked_image.setImageResource(R.drawable.blue_circle);
+        }
     }
 }
